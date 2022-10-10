47b2c4a6fda39051265f6461aae5c12ccdec5e7f
Mark-GeorgeK <79061147+Mark-GeorgeK@users.noreply.github.com>
2022-10-10 17:25:28 +0200

    Handling Input getInputBlocks() function is added which handles input files, conversion to binary and concatenating all bits of a block in one ulli variable. Also, a print function added to check bit streams data bit by bit.

--- DES-Encryption-Algorithm.cpp
+++ DES-Encryption-Algorithm.cpp
@@ -1,25 +1,55 @@
 // DES-Encryption-Algorithm.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+typedef unsigned long long int ulli;
 using namespace std;
 
-int main()
-{
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cout << "Hello" << endl;
-    }
+vector<ulli>* getInputBlocks() {
+	//sample_input contains the HEX representation of "It's Hello World"
+	//contains: 497427732048656c 6c6f20576f726c64
+	ifstream cin("sample_input.txt");
+
+	vector<ulli>* bitStreams = new vector<ulli>;
+	char ch;
+	while (!cin.eof()) {
+		ulli bitStream = 0;
+		int counter = 0;
+		while (counter++ < 16 && cin >> ch) {
+			if (ch >= 65) {
+				ch = toupper(ch);
+				bitStream = bitStream | ((ulli)(ch - 55) << (64 - counter * 4));
+			}
+			else bitStream = bitStream | ((ulli)(ch - 48) << (64 - counter * 4));
+		}
+		if (counter != 1) (*bitStreams).push_back(bitStream);
+	}
+
+	return bitStreams;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void printBitStreams(vector<ulli>* bitStreams) {
+	//logs output to sample_output
+#pragma warning(disable : 4996)
+	auto F = freopen("sample_output.txt", "w", stdout);
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	for (auto bitStream : *bitStreams) {
+		cout << "--Block Started--" << endl;
+		int flag = 1;
+		int c = 63;
+		while (c + 1) {
+			cout << ((bitStream >> c) & flag);
+			if (c % 4 == 0) cout << endl;
+			c--;
+		}
+		cout << "--Block Ended--" << endl;
+	}
+}
+
+int main() {
+
+	vector<ulli>* bitStreams = getInputBlocks();
+	printBitStreams(bitStreams);
+
+}
